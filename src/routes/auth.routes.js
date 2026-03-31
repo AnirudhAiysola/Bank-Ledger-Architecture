7aee7cd47eb913acd1612e8cb16829334b1882b4
anirudhaiysola <anirudhisola1011@gmail.com>
2026-03-31 09:49:44 -0500

    ttl token and transaction fixes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,5 +5,6 @@ const authController = require("../controllers/auth.controller");
 
 router.post("/register", authController.userRegisterController);
 router.post("/login", authController.userLoginController);
+router.post("/logout", authController.userLogoutController);
 
 module.exports = router;
